adeeb75db9db6ec831da1ccc2d58b8a23bc3af77
Will Danner <willdanneriv@gmail.com>
2021-03-29 00:18:36 -0400

    Update WME US Government Boundaries.js
    Forgot to update the version.

--- WME US Government Boundaries.js
+++ WME US Government Boundaries.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            WME US Government Boundaries
 // @namespace       https://greasyfork.org/users/45389
-// @version         2021.08.01.002
+// @version         2021.29.03.001
 // @description     Adds a layer to display US (federal, state, and/or local) boundaries.
 // @author          MapOMatic
 // @include         /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
